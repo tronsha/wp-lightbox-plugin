c604391c12a45066e2299fe4a031f5b1f6d967fd
Stefan Hüsges <tronsha@gmx.de>
2016-04-13 09:44:23 +0200

    version 1.1.5

--- wordpress/trunk/mpcx-lightbox.php
+++ wordpress/trunk/mpcx-lightbox.php
@@ -8,7 +8,7 @@
  * Plugin Name:       Lightbox
  * Plugin URI:        https://github.com/tronsha/wp-lightbox-plugin
  * Description:       Lightbox Plugin
- * Version:           1.1.4
+ * Version:           1.1.5
  * Author:            Stefan Hüsges
  * Author URI:        http://www.mpcx.net/
  * Copyright:         Stefan Hüsges
@@ -27,13 +27,13 @@ if ( ! is_admin() ) {
 				'mpcx-lightbox',
 				plugin_dir_url( __FILE__ ) . 'public/css/lightbox.min.css',
 				array(),
-				'1.1.4'
+				'1.1.5'
 			);
 			wp_register_script(
 				'mpcx-lightbox',
 				plugin_dir_url( __FILE__ ) . 'public/js/lightbox.min.js',
 				array( 'jquery' ),
-				'1.1.4',
+				'1.1.5',
 				true
 			);
 			wp_enqueue_style( 'mpcx-lightbox' );
