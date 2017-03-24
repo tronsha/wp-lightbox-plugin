dc28fc880b205cb3dcd26e430b186aebfd82fa21
Stefan Hüsges <tronsha@gmx.de>
2017-03-24 08:20:18 +0100

    update

---
+++ wordpress/trunk/admin/options.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @link    https://github.com/tronsha/wp-lightbox-plugin
+ * @since   1.1.8
+ * @package wp-lightbox-plugin
+ */
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+?>
+<div class="wrap">
+	<h1>
+		Lightbox
+	</h1>
+</div>
--- wordpress/trunk/mpcx-lightbox.php
+++ wordpress/trunk/mpcx-lightbox.php
@@ -27,7 +27,7 @@ load_plugin_textdomain( 'mpcx-lightbox', false, dirname( plugin_basename( __FILE
 register_activation_hook(
 	__FILE__,
 	function () {
-		add_option( 'mpcx_lightbox', array( 'version' => MPCX_LIGHTBOX_VERSION ) );
+		add_option( 'mpcx_lightbox', array( 'version' => MPCX_LIGHTBOX_VERSION, 'title' => 0 ) );
 	}
 );
 
