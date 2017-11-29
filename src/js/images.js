a51d2873453d3c1b52e397752dad24dbf6c9c772
Stefan Hüsges <tronsha@gmx.de>
2017-11-29 07:10:37 +0100

    Lightbox v2.10.0

--- src/js/images.js
+++ src/js/images.js
@@ -22,44 +22,51 @@ jQuery(document).ready(function () {
                 id = src.split(/[^a-zA-Z0-9]+/).join('-') + '-' + id;
             }
             var $a = $img.parent('a');
+            if (undefined !== $a.attr('data-lightbox') && 'lightbox' !== lbData.lightbox) {
+                $a.attr('data-' + lbData.lightbox, $a.attr('data-lightbox'));
+                $a.removeAttr('data-lightbox');
+            }
+            if (undefined !== $a.attr('data-title') && 'title' !== lbData.title) {
+                $a.attr('data-' + lbData.title, $a.attr('data-title'));
+                $a.removeAttr('data-title');
+            }
             var href = $a.attr('href');
-            if (undefined !== href) {
-                if (null !== href.match(/\.(jpeg|jpg|gif|png)$/)) {
-                    if (undefined === $a.attr('data-' + lbData.lightbox) || '' === $a.attr('data-' + lbData.lightbox)) {
-                        $a.attr('data-' + lbData.lightbox, id);
-                    } 
-                    if (1 === parseInt(lbData.ajax)) {
-                        var classArray = [];
-                        var classAttr = $img.attr('class');
-                        if (undefined !== classAttr) {
-                            classArray = classAttr.split(' ');
-                        }
-                        var postId = '';
-                        for (var i = 0, len = classArray.length; i < len; i++) {
-                            if (0 === classArray[i].indexOf('wp-image-')) {
-                                postId = parseInt(classArray[i].replace('wp-image-', ''));
-                                jQuery.ajax({
-                                    type: 'POST',
-                                    url: lbData.ajaxUrl,
-                                    dataType: 'json',
-                                    data: {
-                                        'postId': postId,
-                                        'action': 'lightbox_get_image_title'
-                                    },
-                                    success: function (result) {
-                                        if ('' !== result) {
-                                            $a.attr('data-' + lbData.title, result);
-                                        }
-                                    },
-                                    error: function () {
+            if (undefined !== href && null !== href.match(/\.(jpeg|jpg|gif|png)$/)) {
+                if (undefined === $a.attr('data-' + lbData.lightbox) || '' === $a.attr('data-' + lbData.lightbox)) {
+                    $a.attr('data-' + lbData.lightbox, id);
+                } 
+                if (1 === parseInt(lbData.ajax)) {
+                    var classArray = [];
+                    var classAttr = $img.attr('class');
+                    if (undefined !== classAttr) {
+                        classArray = classAttr.split(' ');
+                    }
+                    var postId = '';
+                    for (var i = 0, count = classArray.length; i < count; i++) {
+                        if (0 === classArray[i].indexOf('wp-image-')) {
+                            postId = parseInt(classArray[i].replace('wp-image-', ''));
+                            jQuery.ajax({
+                                type: 'POST',
+                                url: lbData.ajaxUrl,
+                                dataType: 'json',
+                                data: {
+                                    'postId': postId,
+                                    'action': 'lightbox_get_image_title'
+                                },
+                                success: function (result) {
+                                    if ('' !== result) {
+                                        $a.attr('data-' + lbData.title, result);
                                     }
-                                });
-                            }
+                                },
+                                error: function () {
+                                }
+                            });
                         }
                     }
                 }
             } else {
                 $a.removeAttr('data-' + lbData.lightbox);
+                $a.removeAttr('data-' + lbData.title);
             }
         }
     });
