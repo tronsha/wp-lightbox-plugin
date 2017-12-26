fd64a211296c3a8030cd58737156dbb36628fa36
Stefan Hüsges <tronsha@gmx.de>
2017-12-26 09:45:00 +0100

    Add csso

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ rm ./wordpress/trunk/public/js/*
 rm ./wordpress/trunk/public/images/*
 ./vendor/uglifyjs -m --comments /^!/ ./src/js/images.js -o ./wordpress/trunk/public/js/images.min.js
 ./vendor/uglifyjs -m --comments /^!/  ./vendor/lightbox/dist/js/lightbox.js -o ./wordpress/trunk/public/js/lightbox.min.js
-java -jar ./vendor/yuicompressor-2.4.8.jar --type css ./vendor/lightbox/dist/css/lightbox.css -o ./wordpress/trunk/public/css/lightbox.min.css
+./vendor/csso ./vendor/lightbox/dist/css/lightbox.css -o ./wordpress/trunk/public/css/lightbox.min.css
 cp ./vendor/lightbox/dist/images/* ./wordpress/trunk/public/images/
 ./vendor/uglifyjs -m --comments /^!/  ./vendor/fancybox/dist/jquery.fancybox.js -o ./wordpress/trunk/public/js/fancybox.min.js
-java -jar ./vendor/yuicompressor-2.4.8.jar --type css ./vendor/fancybox/dist/jquery.fancybox.css -o ./wordpress/trunk/public/css/fancybox.min.css
+./vendor/csso ./vendor/fancybox/dist/jquery.fancybox.css -o ./wordpress/trunk/public/css/fancybox.min.css
 ./vendor/uglifyjs -m --comments /^!/  ./vendor/justifiedgallery/dist/js/jquery.justifiedGallery.js -o ./wordpress/trunk/public/js/justifiedgallery.min.js
-java -jar ./vendor/yuicompressor-2.4.8.jar --type css ./vendor/justifiedgallery/dist/css/justifiedGallery.css -o ./wordpress/trunk/public/css/justifiedgallery.min.css
+./vendor/csso ./vendor/justifiedgallery/dist/css/justifiedGallery.css -o ./wordpress/trunk/public/css/justifiedgallery.min.css
